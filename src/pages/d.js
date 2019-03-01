03ce92c212f1a1dc07af229a639d06fb322d74e4
mamba <1612786059@qq.com>
2019-03-01 19:46:56 +0800

    webpack初试上

---
+++ src/pages/d.js
@@ -0,0 +1,4 @@
+import '../page2.scss'
+const div = document.createElement('div')
+div.innerText = 'D页面'
+document.body.appendChild(div)
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const globby = require('globby');
+const path = require('path')
+
+const paths = globby.sync(['./src/pages/*.js'])
+const entry = {}
+const plugins = []
+paths.map(p => {
+    const name = path.basename(p).split('.').slice(0, -1)[0]
+    entry[name] = p
+    plugins.push(
+        new HtmlWebpackPlugin({
+            filename:`${name}.html`,
+            chunks: [name]
+        }),
+    )
+})
+module.exports = {
+    entry: entry,
+    mode: 'production',
+    plugins: plugins,
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                "style-loader",
+                "css-loader", 
+                "sass-loader"
+            ]
+        }]
+    },
+};
